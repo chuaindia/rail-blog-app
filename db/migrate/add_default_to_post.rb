9466303b939b52fc7d6a507da6487f116994c58e
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2023-01-28 19:41:28 +0530

    Add the new files

--- db/migrate/add_default_to_post.rb
+++
@@ -1,6 +0,0 @@
-class AddDefaultToPost < ActiveRecord::Migration[7.0]
-    def change
-      change_column_default :posts, :CommentCounter, from: nil, to: 0
-      change_column_default :posts, :LikeCounter, from: nil, to: 0
-    end
-  end
--- db/migrate/add_default_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddDefaultToUser < ActiveRecord::Migration[7.0]
-    def change
-      change_column_default :users, :PostCounter, from: nil, to: 0
-    end
-  end
